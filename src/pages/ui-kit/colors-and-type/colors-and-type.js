0a186f6292a401f798a1dbf545f779d656c8402b
KlimanovNikita <klimanovnik98@gmail.com>
2021-07-12 14:18:10 +0300

    Some changes on Colors&Type and index pages

--- src/pages/ui-kit/colors-and-type/colors-and-type.js
+++ src/pages/ui-kit/colors-and-type/colors-and-type.js
@@ -6,5 +6,3 @@ import "./colors-and-type.scss";
 // Components
 import "./../../../components/header-with-pale-logo/header-with-pale-logo.js";
 import "./../../../components/_container-1440/container-1440.js";
-import "./../../../components/square-with-text/square-with-text.js";
-import "./../../../components/title-and-subtitle/title-and-subtitle.js";
