0da09d030788e0cfacb308a7353ffbb162e7ffac
KlimanovNikita <klimanovnik98@gmail.com>
2021-08-03 18:55:59 +0300

    Finished Landing page

--- src/pages/website-pages/landing-page/landing-page.js
+++ src/pages/website-pages/landing-page/landing-page.js
@@ -3,8 +3,55 @@ import "./../../../assets/fonts.scss";
 import "./../../../assets/reset.scss";
 
 // Components
-import "./../../../components/_container-1440/container-1440.js";
-import "./../../../components/header-with-pale-logo/header-with-pale-logo.js";
+import "./../../../components/_container-1160/container-1160.js";
+import "./../../../components/header/header.js";
+import "./../../../../node_modules/slick-carousel/slick/slick.min.js";
+import "./../../../../node_modules/slick-carousel/slick/slick.css";
+import "./../../../../node_modules/slick-carousel/slick/slick-theme.css";
+import "./../../../components/pick-up-room/pick-up-room.js";
+import "./../../../components/footer/footer.js";
 
 // Page
 import "./landing-page.scss";
+
+function importAll(r) {
+    let images = {};
+    r.keys().map(function (item) {
+        images[item.replace("./", "")] = r(item);
+    });
+    return images;
+}
+
+const images = importAll(require.context("./img", false, /\.(ico|gif|png|jpg|jpeg|svg)$/));
+
+$(function () {
+
+    let imgBlockArray = $(".landing-page__auto-slider-img");
+    let imgModule = "";
+
+    for (let imgBlock of imgBlockArray) {
+        imgModule = images[imgBlock.alt];
+
+        if (imgModule === undefined) {
+            imgModule = images["bg-1.jpg"];
+        }
+
+        imgBlock.setAttribute("src", `${imgModule.default}`);
+    }
+
+    $(".landing-page__auto-slider").slick({
+        infinite: true,
+        draggable: false,
+        arrows: false,
+        dots: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        pauseOnFocus: false,
+        pauseOnHover: false,
+        fade: true,
+        speed: 5000
+    });
+
+});
