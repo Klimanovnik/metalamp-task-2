fa076b80432eaa1d6d2a53894064f6537e9b6bbe
KlimanovNikita <klimanovnik98@gmail.com>
2021-08-05 19:12:28 +0300

    Began creating the Search room page

--- src/pages/website-pages/search-room/search-room.js
+++ src/pages/website-pages/search-room/search-room.js
@@ -1,9 +1,17 @@
 // Assets
 import "./../../../assets/fonts.scss";
 import "./../../../assets/reset.scss";
+import "./../../../assets/container-1920.scss";
 
 // Components
 import "./../../../components/header/header.js";
+import "./../../../components/filter-date-dropdown/filter-date-dropdown.js";
+import "./../../../components/dropdown/dropdown.js";
+import "./../../../components/range-slider/range-slider.js";
+import "./../../../components/checkbox/checkbox.js";
+import "./../../../components/expandable-checkbox-list/expandable-checkbox-list.js";
+import "./../../../components/room-card/room-card.js";
+import "./../../../components/pagination/pagination.js";
 import "./../../../components/footer/footer.js";
 
 // Page
